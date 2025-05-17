c94685a0cedd31b6c88df489f50a00851563845c
mentalri <t.ritterhoff-pv@gmx.de>
2025-05-17 12:00:01 +0200

    Enhance profile page layout with new styling and structure for user data

--- php/view/profile_konto.php
+++ php/view/profile_konto.php
@@ -16,47 +16,84 @@ $cssFiles = array("pages/profil.css","pages/profil-sidebar.css","pages/profil_ko
             <section class="content flex-column flex-grow">
             <h2>Konto</h2>
                 <div class="konto-container">
-                    <div class="konto-header">
-                        <h3>Kontoinformationen</h3>
-                        
+                    <div class="login-data-container">
+                        <div class="konto-header">
+                            <h3>Anmeldedaten</h3>
+
+                        </div>
+                        <form class="konto-info">
+                            <div class="konto-field">
+                                <label for="username">Benutzername:</label>
+                                <div>
+                                    <input type="text" id="username" value=<?=$profile->getUsername()?>>
+                                </div>
+                            </div>
+                            <div class="konto-field">
+                                <label for="email">E-Mail:</label>
+                                <div>
+                                    <input type="email" id="email" value=<?=$profile->getEmail()?>>
+                                </div>
+                            </div>
+                            <div class="konto-field">
+                                <label for="current_password">Aktuelles Passwort:</label>
+                                <div>
+                                    <input type="password" id="current_password" value="********" disabled>
+                                </div>
+                            </div>
+                            <div class="konto-field">
+                                <label for="new_password">Neues Passwort:</label>
+                                <div>
+                                    <input type="password" id="new_password" value="">
+                                </div>
+                            </div>
+                            <div class="konto-field">
+                                <label for="repeat_password">Passwort bestätigen:</label>
+                                <div>
+                                    <input type="password" id="repeat_password" value="">
+                                </div>
+                            </div>
+                        </form>
+                        <button type="submit" class="delete-button">Profil löschen</button>
+                        <button type="submit" class="display_password">Passwort anzeigen</button>
+                        <button type="submit" class="save-button">Speichern</button>
+                        <button type="submit" class="cancel-button">Abbrechen</button>
                     </div>
-                    <form class="konto-info">
-                        <div class="konto-field">
-                            <label for="username">Benutzername:</label>            
-                            <div>
-                                <input type="text" id="username" value=<?=$profile->getUsername()?>>                                
-                            </div>   
-                        </div>
-                        <div class="konto-field">
-                            <label for="email">E-Mail:</label>            
-                            <div>
-                                <input type="email" id="email" value=<?=$profile->getEmail()?>>                                
-                            </div>   
-                        </div>
-                        <div class="konto-field">
-                            <label for="current_password">Aktuelles Passwort:</label>
-                            <div>
-                                <input type="password" id="current_password" value="********" disabled>                                
-                            </div>            
-                        </div>
-                        <div class="konto-field">
-                            <label for="new_password">Neues Passwort:</label>
-                            <div>
-                                <input type="password" id="new_password" value="">                                
+                    <div class="profile-data-container">
+                        <div class="profile-data-header">
+                            <div class="profile-picture">
+                                <img src="<?=$profile->getProfilePicture()?>" alt="Profilbild">
+                            </div>
+                            <input type="checkbox" name="action-menu-toggle-box" id="action-menu-toggle-box">
+                            <label for="action-menu-toggle-box" class="action-menu-toggle">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </label>
+                            <div class="action-menu">
+                                <label>Profilbild ändern</label>
+                                <a href="#">Profilbild löschen</a>
+                                <label>Beschreibung ändern</label>
+                            </div>
+                            <div class="profile-meta">
+                                <div class="meta-header">
+                                    <h3 class="username"><?= htmlspecialchars($profile->getUsername()) ?></h3>
+                                </div>
+                                <div class="meta-body">
+                                    <p><?=count($profile->getFollowers())?> Follower</p>
+                                    <p><?=count($profile->getFollowing())?> gefolgt</p>
+                                    <p><?=count($profile->getReports())?> Berichte</p>
+                                </div>
                             </div>
                         </div>
-                        <div class="konto-field">
-                            <label for="repeat_password">Passwort bestätigen:</label>
-                            <div>
-                                <input type="password" id="repeat_password" value="">
-                            </div>            
+
+                        <div class="description">
+                            <h3>Beschreibung</h3>
+                            <p>Hier kannst du eine kurze Beschreibung über dich hinzufügen.</p>
+
                         </div>
-                    </form>
-                    <button type="submit" class="delete-button">Profil löschen</button>
-                    <button type="submit" class="display_password">Passwort anzeigen</button>
-                    <button type="submit" class="save-button">Speichern</button>
-                    <button type="submit" class="cancel-button">Abbrechen</button>
-                </div>        
+                    </div>
+                </div>
+
             </section>
         </main>
         <?php include_once $abs_path."/php/include/footer.php" ?>
